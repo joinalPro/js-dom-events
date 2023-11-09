60295b84322ec8b4d47c623c7df789457d8be98d
joinalzx <joinalzx@gmail.com>
2023-11-09 18:06:33 +0600

    dom events

--- js/events.js
+++ js/events.js
@@ -1,1 +1,25 @@
 console.log('this is seperate file')
+
+//option 1 diretly set element
+//<button onclick="document.body.style.backgroundColor = 'Yellow'">Make Yellow</button>
+//option 2: add onclick function
+//<button onclick="makeRed()">Make Red</button>
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+//option 3
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+//option 3 another:
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple(){
+ document.body.style.backgroundColor = 'purple';
+}
+
+//option 4:
